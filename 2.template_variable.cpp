a5b946298a9a9348a101996cd7617059e18a86b5
Vaibhav Chhabra <vaibhavarena@gmail.com>
2021-04-20 18:45:27 +0530

    template variable

--- 2.template_variable.cpp
+++ 2.template_variable.cpp
@@ -1,16 +1,5 @@
-## Introduction
+#include <iostream>
 
-- templating works like macros and substitute the code during compile time, but also does type checking
-
-- typename and class can be interchangeable
-```c++
-template <typename T>
-template <class T>
-```
-
-#### template variable
-    Available from C++ 14
-```c++
 template <typename T>
 T pi = T(3.1415926535897932385L);
 
@@ -27,4 +16,3 @@ int main()
     std::cout << areaCircle<long double>(4) << "\n";
     std::cout << areaCircle<double>(4) << "\n"; // Will give correct output to 16 places only
 }
-```
